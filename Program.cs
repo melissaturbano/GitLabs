a0678130eddd00542a13e18766c900fde47f375b
Melissa Turbano <melissaturbano@gmail.com>
2022-05-31 16:50:26 -0300

    operacao somar

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var operacao = args [0];
+var n1 = Convert.ToDouble(args[1]);
+var n2 = Convert.ToDouble(args[2]);
+
+
+
+if (operacao == "Somar")
+{
+    Console.WriteLine(n1+n2);
+}
+
